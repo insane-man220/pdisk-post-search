d76d7240e6c428944b2386a4348427ab6c478b52
insane-man220 <76524189+insane-man220@users.noreply.github.com>
2021-11-07 21:52:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,6 @@ async def get_ptitle(url):
         pass
     title = list(title.get_text())
     title = title[8:]
-    str = 't.me/' + CHANNEL + ' '
     for i in title:
         str = str + i
     lst = list(html_text.split(","))
@@ -94,7 +93,7 @@ async def pdisk_up(link):
         title_new = os.path.basename(title_new.path)
         title_pdisk = title_new  +' '+ '@' + CHANNEL
     res = requests.get(
-        'http://linkapi.net/open/create_item?link_type=link&content_src=' + link + '&source=2000&cover_url='+THUMB_URL+'&api_key=' + PDISK_API_KEY + '&dir_id=0&title=' + title_pdisk + '&description=Join_' JOIN US ON TELEGRAM '@'+ + CHANNEL + '_for_more_like_this')
+        'http://linkapi.net/open/create_item?link_type=link&content_src=' + link + '&source=2000&cover_url='+THUMB_URL+'&api_key=' + PDISK_API_KEY + '&dir_id=0&title=' + title_pdisk + '&description=Join_' JOIN US ON TELEGRAM '@' + CHANNEL + '_for_more_like_this')
     data = res.json()
     data = dict(data)
     print(data)
